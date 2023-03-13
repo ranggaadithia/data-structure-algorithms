497338a23a36b1633d7ae87309e0292c15a8224e
Rangga Adithia <rangga.adithia26@gmail.com>
2023-03-13 20:07:53 +0800

    finish program

--- Binary Seach/app.cpp
+++ Binary Seach/app.cpp
@@ -3,6 +3,29 @@ using namespace std;
 
 int main()
 {
-  cout << "Hello Rangga!";
+  int arr[] = {1, 3, 5, 7, 9, 11};
+  int sizeArray = sizeof(arr) / sizeof(arr[0]);
+  int target = 11;
+  int left = 0, right = sizeArray - 1;
+
+  while (left <= right)
+  {
+    int mid = left + (right - left) / 2;
+
+    if (arr[mid] == target)
+    {
+      cout << "Nilai ditemukan pada indeks ke-" << mid << endl;
+      return 0;
+    }
+    else if (arr[mid] < target)
+    {
+      left = mid + 1;
+    }
+    else
+    {
+      right = mid - 1;
+    }
+  }
+
   return 0;
 }
