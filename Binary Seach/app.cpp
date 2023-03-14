1938d695f350bc3ee13b8061133ca8470c58075c
Rangga Adithia <rangga.adithia26@gmail.com>
2023-03-14 20:40:09 +0800

    make function & error prevention

--- Binary Seach/app.cpp
+++ Binary Seach/app.cpp
@@ -1,13 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int main()
+int binarySearch(int arr[], int target, int sizeArray)
 {
-  int arr[] = {1, 3, 5, 7, 9, 11};
-  int target;
-  int sizeArray = sizeof(arr) / sizeof(arr[0]);
-  cout << "Masukan Nomor yang ingin dicari : ";
-  cin >> target;
   int left = 0, right = sizeArray - 1;
 
   while (left <= right)
@@ -16,8 +11,7 @@ int main()
 
     if (arr[mid] == target)
     {
-      cout << "Nilai ditemukan pada indeks ke-" << mid << endl;
-      return 0;
+      return mid;
     }
     else if (arr[mid] < target)
     {
@@ -27,6 +21,28 @@ int main()
     {
       right = mid - 1;
     }
+  }
+
+  return false;
+}
+
+int main()
+{
+  int arr[] = {1, 3, 5, 7, 9, 11};
+  int target;
+  int sizeArray = sizeof(arr) / sizeof(arr[0]);
+  cout << "Input Number : ";
+  cin >> target;
+
+  int result = binarySearch(arr, target, sizeArray);
+
+  if (result == false)
+  {
+    cout << "Element not found" << endl;
+  }
+  else
+  {
+    cout << "Element found at index -" << result << endl;
   }
 
   return 0;
